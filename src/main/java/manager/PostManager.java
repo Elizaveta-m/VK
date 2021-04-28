d915f30972329c119581ca22258e8439f34ebc4c
Elizaveta Morozova <rusquee@gmail.com>
2021-04-28 03:59:13 +0300

    PostManager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,13 @@
+package main.java.manager;
+
+import main.java.domain.Post;
+
+public class PostManager {
+    private Post posts;
+
+    public Post search(int ownerId, String domain, String query, int ownerOnly, int count, int offset) {
+        return posts;
+    }
+    public void delete(int postId, int ownerId) {
+    }
+}
